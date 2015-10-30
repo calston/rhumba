9729d67e35e9cc792ea771c80573cee8a6cc36f9
Colin Alston <colin.alston@gmail.com>
2015-10-30 16:25:36 +0200

    Working version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="rhumba",
-    version='0.0.1',
+    version='0.1.0',
     url='http://github.com/calston/rhumba',
     license='MIT',
     description="An asynchronous job queue written in Twisted",
