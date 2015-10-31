c321e181c11348db15f6343d815d167164b60af4
Colin Alston <colin.alston@gmail.com>
2015-10-31 19:56:56 +0200

    Add a twisted client

--- rhumba/client.py
+++ rhumba/client.py
@@ -3,6 +3,86 @@ import time
 import json
 import uuid
 
+from txredis.client import RedisClient
+
+class AsyncRhumbaClient(object):
+    """
+    Rhumba client
+    """
+    def __init__(self, host='localhost', port=6379, db=0):
+        self.redis_host = host
+        self.redis_port = port
+        self.db = db
+
+        reactor.callWhenRunning(self.connect)
+
+    @defer.inlineCallbacks
+    def connect(self):
+        clientCreator = protocol.ClientCreator(reactor, RedisClient, db=self.db)
+
+        self.client = yield clientCreator.connectTCP(
+            self.redis_host, self.redis_port
+        )
+
+    @defer.inlineCallbacks
+    def queue(self, queue, message, params={}):
+        """
+        Queue a job in Rhumba
+        """
+        d = {
+            'id': uuid.uuid1().get_hex(),
+            'version': 1,
+            'message': message,
+            'params': params
+        }
+
+        yield self.client.lpush('rhumba.q.%s' % queue, json.dumps(d))
+
+        defer.returnValue(d['id'])
+    
+    @defer.inlineCallbacks
+    def getResult(self, queue, uid):
+        """
+        Retrieve the result of a job from its ID
+        """
+        r = yield self.client.get('rhumba.q.%s.%s' % (queue, uid))
+
+        if r:
+            defer.returnValue(json.loads(r))
+        else:
+            defer.returnValue(None)
+
+    @defer.inlineCallbacks
+    def clusterStatus(self):
+        """
+        Returns a dict of cluster nodes and their status information
+        """
+        servers = self.client.keys('rhumba.server.*.heartbeat')
+        
+        d = {}
+
+        now = time.time()
+
+        for s in servers:
+            sname = s.split('.', 2)[-1].rsplit('.', 1)[0]
+            
+            last = yield self.client.get('rhumba.server.%s.heartbeat' % sname)
+
+            if not last:
+                last = 0
+
+            status = yield self.client..get('rhumba.server.%s.status' % sname)
+
+            if (status == 'ready') and (now - last > 5):
+                status = 'offline'
+
+            d[sname] = {
+                'lastseen': last,
+                'status': status
+            }
+
+        defer.returnValue(d)
+
 class RhumbaClient(object):
     """
     Rhumba client
