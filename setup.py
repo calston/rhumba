2384238c808bf564e1d04e1b6d5ffdbfe9f72d28
Colin Alston <colin.alston@gmail.com>
2015-10-31 20:19:06 +0200

    Bump version number

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="rhumba",
-    version='0.1.0',
+    version='0.1.1',
     url='http://github.com/calston/rhumba',
     license='MIT',
     description="An asynchronous job queue written in Twisted",
