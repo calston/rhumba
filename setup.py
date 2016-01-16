c86606eecb93bc0e5333eb8de2e3d110e3db6b42
Colin Alston <colin.alston@gmail.com>
2016-01-16 10:48:27 +0200

    Dodgey but lets bump the version and try..

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="rhumba",
-    version='0.1.16',
+    version='0.1.17',
     url='http://github.com/calston/rhumba',
     license='MIT',
     description="An asynchronous job queue written in Twisted",
